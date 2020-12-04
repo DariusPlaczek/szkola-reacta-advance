43852dc7444ba53d94fedf1a6ccf2c3a9d40d912
Darius Placzek <dariusplaczek@gmail.com>
2020-12-04 13:34:58 +0100

    configuration and inplation leaflet

--- src/components/Pages/User/UserDetails.js
+++ src/components/Pages/User/UserDetails.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useLocation} from 'react-router-dom';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "react-placeholder/lib/reactPlaceholder.css";
 
 function UserDetails() {
@@ -46,9 +46,12 @@ function UserDetails() {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position} />
+            <Marker position={position}>
+              <Popup>
+                I Life here. <br /> Nice Place.
+              </Popup>
+            </Marker>
           </MapContainer>
-
         </>
     )
 }
